55e4a5696ccf4df54563e2c840533cb8b23ff3fe
Kudzanayi Dzvairo <kudzanayi@Kudzanayis-MacBook-Air.local>
2019-02-19 23:13:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+function domContentLoaded(){
+
+document.addEventListener("DOMContentLoaded", updateDOM)
+const text = document.querySelector('#text')
+
+}
+
+function updateDOM() {
+text.innerHTML = 'This is really cool!'
+}
